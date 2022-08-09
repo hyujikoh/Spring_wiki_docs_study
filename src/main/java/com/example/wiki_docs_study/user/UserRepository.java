8bc605565e698c9ff2a6bc95bc1595c953dfa98e
hyujikoh <dvum0045@gmail.com>
2022-08-09 15:32:46 +0900

    회원가입 api 완료

--- src/main/java/com/example/wiki_docs_study/user/UserRepository.java
+++ src/main/java/com/example/wiki_docs_study/user/UserRepository.java
@@ -1,4 +1,6 @@
 package com.example.wiki_docs_study.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
 }
