e5b048d81a4bf8ec990244470b79db69545c2903
hyujikoh <dvum0045@gmail.com>
2022-08-09 15:36:42 +0900

    회원가입 도중 중복 아이디인지 여부 확인

--- src/main/java/com/example/wiki_docs_study/user/UserController.java
+++ src/main/java/com/example/wiki_docs_study/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.wiki_docs_study.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,10 +36,19 @@ public class UserController {
                     "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
         }
-
-        userService.create(userCreateForm.getUsername(),
-                userCreateForm.getEmail(), userCreateForm.getPassword1());
-
+        // 이메일 도메인은 unique 하기때문에 해당 속성을 처리 하기 위한 중복회원가입여부 확인
+        try {
+            userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        }catch(Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
         return "redirect:/";
     }
 }
